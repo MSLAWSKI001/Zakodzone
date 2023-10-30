b2dc8a20a5bcd0a84ff4fc03345ede89861efd80
MSLAWSKI001 <113010458+MSLAWSKI001@users.noreply.github.com>
2023-10-30 15:10:35 +0100

    Add files via upload

---
+++ ms26/urzytkownicy.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Portal społecznościowy</title>
+</head>
+<body>
+    <link rel="stylesheet" href="styl5.css">
+    <header class ="hlewy">
+    <h2>Nasze osiedle</h2>
+    </header>
+    <header class ="hprawy">
+
+    </header>
+    <aside class = "alewy">
+    <h3>Logowanie</h3>
+    <form  method="post">
+        login<input type="text" name="login" id="login"><br>
+        haslo<input type="password" name="haslo" id="haslo"><br>
+        <button type="submit">Zaloguj</button>
+    </form>
+    </aside>
+    <aside class = "aprawy">
+    <h3>Wizytówka</h3>
+    <div id = "wizytówka">
+        <button id = "przyciskwiz">Więcej informacji</button>
+    </div>
+    </aside>
+    <footer>
+        <p>Stronę wykonał ez</p>
+    </footer>
+</body>
+</html>
