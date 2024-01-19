fd407c0984cf006e18be6125b9c081f26c397f7d
MSLAWSKI001 <113010458+MSLAWSKI001@users.noreply.github.com>
2024-01-19 09:19:28 +0100

    Add files via upload

---
+++ 05282907694/main.js
@@ -0,0 +1,7 @@
+function main() {
+    var imie = document.getElementById(imie).value
+    var nazwisko = document.getElementById(nazwisko).value;
+    var email = document.getElementById(email).value.toLowerCase();
+    var wybor =  document.getElementById(wybor);
+}
+var wynik = element.innerHTML = (value(imie) + " " + value(nazwisko) + value(email));
